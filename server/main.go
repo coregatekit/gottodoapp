d199490637bf03e2a4cbebdb68b166065690b1b4
Joe Kim <joesm.genie@gmail.com>
2024-06-26 14:51:37 +0700

    Implement basic router

--- server/main.go
+++ server/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"github.com/coregatekit/gotodoapp/routers"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
-	r.Run()
+	api := r.Group("/api")
+	routers.RouterRegister(api)
+
+	r.Run(":8080")
 }
---
+++ server/routers/app_route.go
@@ -0,0 +1,11 @@
+package routers
+
+import "github.com/gin-gonic/gin"
+
+func RouterRegister(router *gin.RouterGroup) {
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+}
